ff448ce056eb416497f8e57d6c7e061d5478a380
Sunny Singh <154250275+sunny-singh165@users.noreply.github.com>
2025-05-04 22:03:30 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: 144.76.114.186,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
