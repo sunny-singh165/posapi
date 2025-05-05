00130585b8abb1f97d128077078c762f274ce05a
Sunny Singh <154250275+sunny-singh165@users.noreply.github.com>
2025-05-05 07:55:56 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: 144.76.114.186,
-  user: posinventory_sunposdbuser,
-  password: sunposdbuser@@2025,
-  database: posinventory_sunposdb,
+  host: 'sql12.freesqldatabase.com',
+  user: 'sql12776945',
+  password: '6mkHyKCJFq',
+  database: 'sql12776945',
 });
 
 module.exports = pool.promise();  // So we can use async/await
