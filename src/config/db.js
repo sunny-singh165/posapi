928757604f16a955fadd251c5b70d14964a805c7
Sunny Singh <154250275+sunny-singh165@users.noreply.github.com>
2025-05-29 21:50:31 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,8 @@ const pool = mysql.createPool({
   host: pos.massways.com,
   user: masswayscom_posdbuser,
   password: posdb@2025,
-  database: masswayscom_posdb
+  database: masswayscom_posdb,
+  port: 3306
 });
 
 module.exports = pool.promise();  // So we can use async/await
